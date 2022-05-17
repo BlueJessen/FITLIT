6a5a6b311550bf70109f2515051b1277254cc93b
Blue Jessen <canealisblue@gmail.com>
2022-05-17 14:08:43 -0700

    Refactor user repo test to include hooks

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -3,18 +3,11 @@ import UserRepository from '../src/UserRepository.js';
 import User from '../src/User.js'
 
 describe('User Repository', () => {
-  it('should be a function', function () {
-    expect(UserRepository).to.be.a('function');
-  });
-
-  it('should hold user data', () => {
-    const users = [new User({user: "info"}), new User({user: "test"}), new User({user: "another"})];
-    const userRepo = new UserRepository(users);
-    expect(userRepo.userData).to.deep.equal(users);
-  });
-
-  it('should have a method to find user data by id', () => {
-    const user2 = {
+  let user = null;
+  let user2 = null;
+  let users = null;
+  let userRepo = null;
+    beforeEach(() => {user = new User({
         "id": 2,
         "name": "Jarvis Considine",
         "address": "30086 Kathryn Port, Ciceroland NE 07273",
@@ -22,42 +15,34 @@ describe('User Repository', () => {
         "strideLength": 4.5,
         "dailyStepGoal": 5000,
         "friends": [9, 18, 24, 19]
-      }
-    const user2Class = new User(user2);
-    const userRepo = new UserRepository([user2Class]);
-    expect(userRepo.findUser(2)).to.deep.equal(user2Class);
+      });
+
+      user2 = new User({
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [ 16, 4, 8 ]
+        });
+        users = [user, user2];
+        userRepo = new UserRepository(users);
+  });
+
+  it('should be a function', function () {
+    expect(UserRepository).to.be.a('function');
+  });
+
+  it('should hold user data', () => {
+    expect(userRepo.userData).to.deep.equal(users);
+  });
+
+  it('should have a method to find user data by id', () => {
+    expect(userRepo.findUser(2)).to.deep.equal(user);
   });
 
   it('should have a method to check the average step goal amongst users', () => {
-      const testData = [new User({
-              "id": 2,
-              "name": "Jarvis Considine",
-              "address": "30086 Kathryn Port, Ciceroland NE 07273",
-              "email": "Dimitri.Bechtelar11@gmail.com",
-              "strideLength": 4.5,
-              "dailyStepGoal": 5000,
-              "friends": [9, 18, 24, 19]
-            }),
-            new User({
-                    "id": 3,
-                    "name": "Jarvis Considine",
-                    "address": "30086 Kathryn Port, Ciceroland NE 07273",
-                    "email": "Dimitri.Bechtelar11@gmail.com",
-                    "strideLength": 4.5,
-                    "dailyStepGoal": 7000,
-                    "friends": [9, 18, 24, 19]
-                  }),
-            new User({
-                    "id": 4,
-                    "name": "Jarvis Considine",
-                    "address": "30086 Kathryn Port, Ciceroland NE 07273",
-                    "email": "Dimitri.Bechtelar11@gmail.com",
-                    "strideLength": 4.5,
-                    "dailyStepGoal": 2000,
-                    "friends": [9, 18, 24, 19]
-                    })];
-      const userRepo = new UserRepository(testData);
-
-      expect(userRepo.getAverage()).to.equal(4666);
+      expect(userRepo.getAverage()).to.equal(7500);
   });
 });
