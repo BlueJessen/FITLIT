85c0a65d88992d1cfdde6d6e4fbea3aa0df430de
Neil Linton <lintonbneil@gmail.com>
2022-05-17 16:31:40 -0700

    Add function which helps access each user

--- src/scripts.js
+++ src/scripts.js
@@ -8,21 +8,23 @@ import User from './User';
 // Do not delete or rename this file ********
 var userCard = document.querySelector('.user-card');
 var name = document.querySelector('#name');
-var user = new User({
-    "id": 1,
-    "name": "Luisa Hane",
-    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-    "email": "Diana.Hayes1@hotmail.com",
-    "strideLength": 4.3,
-    "dailyStepGoal": 10000,
-    "friends": [
-      16,
-      4,
-      8
-    ]
-  }
-)
+var userArray = userData.map(user => {
+    return new User(user)
+});
+
+var userRepo = new UserRepository(userArray)
+
+var user = getRandomUser(userRepo.userData);
 //event listener
+
+
+function getRandomUser(array) {
+  var randomIndex = Math.floor(Math.random() * array.length)
+    return array[randomIndex]
+};
+
+
+console.log("REPO", userRepo)
 
 
 
@@ -32,8 +34,11 @@ var user = new User({
 // 3) Using iterator loop to properly filter through each instance-selecting one by their assigned ID
 // 4) Implementing data through innerText/innerHTML
 // 5) the Char should hold a User instance data
+function welcomeUser() {
+  name.innerHTML = user.returnUserName();
+}
 
-
+welcomeUser();
 
 function userToDisplay() {
   userCard.innerHTML =  `<div>${user.id}</div>
