7f2c72ec9e778b71178e5a78b384df983506e1bf
Neil Linton <lintonbneil@gmail.com>
2022-05-17 16:03:37 -0700

    Add function which displays user card

--- src/scripts.js
+++ src/scripts.js
@@ -8,6 +8,20 @@ import User from './User';
 // Do not delete or rename this file ********
 var userCard = document.querySelector('.user-card');
 var name = document.querySelector('#name');
+var user = new User({
+    "id": 1,
+    "name": "Luisa Hane",
+    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    "email": "Diana.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  }
+)
 //event listener
 
 
@@ -22,7 +36,13 @@ var name = document.querySelector('#name');
 
 
 function userToDisplay() {
-  userCard.innerText = 'hello world'
+  userCard.innerHTML =  `<div>${user.id}</div>
+                <div>${user.name}</div>
+                <div>${user.address}</div>
+                <div>${user.email}</div>
+                <div>${user.strideLength}</div>
+                <div>${user.dailyStepGoal}</div>
+                <div>${user.friends}</div>`;
 };
 
 userToDisplay();
