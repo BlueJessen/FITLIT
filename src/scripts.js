7642b6fd68c8fa3244a6bfd5be8b8e676ebb034e
David Daw <daviddaw@gmail.com>
2022-05-18 12:25:42 -0700

    Refactor functions to clarify interaction with fetch call

--- src/scripts.js
+++ src/scripts.js
@@ -1,39 +1,10 @@
 import Chart from 'chart.js/auto';
 import UserRepository from './UserRepository';
 import User from './User';
-// import getPromise from './apiCalls';
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-
 var userCard = document.querySelector('.user-card');
 var name = document.querySelector('#name');
-
-fetch('https://fitlit-api.herokuapp.com/api/v1/users').then(response => response.json()).then(data => {
-
-  let userArray = data.userData.map(person => new User(person));
-  let userRepo = new UserRepository(userArray);
-  let user = getRandomUser(userRepo.userData);
-  userCard.innerHTML =  `<div> Id: ${user.id}</div>
-                <div> Name: ${user.name}</div>
-                <div> Address: ${user.address}</div>
-                <div> Email: ${user.email}</div>
-                <div> Stride Count: ${user.strideLength}</div>
-                <div> Daily Step Goal: ${user.dailyStepGoal}</div>
-                <div> Friends: ${user.friends}</div>
-                <div> Average Step Goal: ${userRepo.getAverage()}</div>`;
-});
-
-
-//   userArray = data.userData.map((person) => {
-//  return new User(person)
-// }));
-// console.log(userArray);
-//userData.map(user => {return new User(user)});
-
-// var userRepo = new UserRepository(userArray)
-// console.log(userRepo);
-// var user = getRandomUser(userRepo.userData);
-//event listener
 
 
 function getRandomUser(array) {
@@ -41,40 +12,30 @@ function getRandomUser(array) {
     return array[randomIndex]
 };
 
-
-console.log("REPO", userRepo)
-
-
-
-//INPUT:
-// 1) Class new UserRepository which holds the User class
-// 2) Passing into function
-// 3) Using iterator loop to properly filter through each instance-selecting one by their assigned ID
-// 4) Implementing data through innerText/innerHTML
-// 5) the Char should hold a User instance data
-function welcomeUser() {
-  console.log(user);
+function userToDisplay(user, userRepo) {
   name.innerHTML = user.returnUserName();
-}
-
-welcomeUser();
-
-function userToDisplay() {
-  userCard.innerHTML =  `<div> Id: ${user.id}</div>
-                <div> Name: ${user.name}</div>
-                <div> Address: ${user.address}</div>
-                <div> Email: ${user.email}</div>
-                <div> Stride Count: ${user.strideLength}</div>
-                <div> Daily Step Goal: ${user.dailyStepGoal}</div>
-                <div> Friends: ${user.friends}</div>
-                <div> Average Step Goal: ${userRepo.getAverage()}</div>`;
-
+  userCard.innerHTML = ``;
+  userCard.innerHTML +=  `<div> Id: ${user.id}</div>`;
+  userCard.innerHTML +=  `<div> Address: ${user.address}</div>`;
+  userCard.innerHTML +=  `<div> Email: ${user.email}</div>`;
+  userCard.innerHTML +=  `<div> Stride Count: ${user.strideLength}</div>`;
+  userCard.innerHTML +=  `<div> Daily Step Goal: ${user.dailyStepGoal}</div>`;
+  userCard.innerHTML +=  `<div> Friends: ${user.friends}</div>`;
+  userCard.innerHTML +=  `<div> Name: ${user.name}</div>`;
+  userCard.innerHTML +=  `<div> Average Step Goal: ${userRepo.getAverage()}</div>`;
 };
 
-userToDisplay();
+function initialSetup() {
+  fetch('https://fitlit-api.herokuapp.com/api/v1/users').then(response => response.json()).then(data => {
+    let userArray = data.userData.map(person => new User(person));
+    let userRepo = new UserRepository(userArray);
+    let user = getRandomUser(userRepo.userData);
+    userToDisplay(user, userRepo);
+  });
+}
 
+initialSetup();
 
-console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 
