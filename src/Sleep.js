ca49baaedfd113330c78212a90634a9880775526
David Daw <daviddaw@gmail.com>
2022-05-19 16:18:00 -0700

    Add Sleep class that passes all tests

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,5 +1,61 @@
 class Sleep {
+  constructor(sleepData) {
+    this.sleepData = sleepData;
+  };
 
-}
+  findUser(userID) {
+    return this.sleepData.find(user => user.userID === userID);
+  };
+
+  findAllUserData(userID) {
+    return this.sleepData.filter(user => user.userID === userID);
+  };
+
+  findAverageSleepHours(userID) {
+    const userData = this.findAllUserData(userID);
+    const averageSleepHours = userData.reduce((acc, dataEntry) => {
+      acc += dataEntry.hoursSlept;
+      return acc
+    }, 0)/userData.length;
+    return Number(averageSleepHours.toFixed(1));
+  };
+
+  findAverageSleepQuality(userID) {
+    const userData = this.findAllUserData(userID);
+    const averageSleepQuality = userData.reduce((acc, dataEntry) => {
+      acc += dataEntry.sleepQuality;
+      return acc
+    }, 0)/userData.length;
+    return Number(averageSleepQuality.toFixed(1));
+  };
+
+  findDaySleepHours(userID, date) {
+    return this.sleepData.find(userData => userID === userData.userID && date === userData.date).hoursSlept;
+  };
+
+  findDaySleepQuality(userID, date) {
+    return this.sleepData.find(userData => userID === userData.userID && date === userData.date).sleepQuality;
+  };
+
+  findWeeklySleepHours(userID, date) {
+    const userData = this.findAllUserData(userID);
+    const dayIndex = userData.findIndex(dataEntry => dataEntry.date === date);
+    const toDate = userData.slice(0 , (dayIndex+1));
+    return toDate.slice(-7).map(date => date.hoursSlept);
+  };
+
+  findWeeklySleepQuality(userID, date) {
+    const userData = this.findAllUserData(userID);
+    const dayIndex = userData.findIndex(dataEntry => dataEntry.date === date);
+    const toDate = userData.slice(0,(dayIndex+1));
+    return toDate.slice(-7).map(date => date.sleepQuality);
+  };
+
+  findAllAverageSleepQuality() {
+    let averageSleepQuality = this.sleepData.reduce((acc, dataEntry) => acc+=dataEntry.sleepQuality, 0)/this.sleepData.length;
+    return Number(averageSleepQuality.toFixed(1))
+  };
+
+};
 
 export default Sleep;
