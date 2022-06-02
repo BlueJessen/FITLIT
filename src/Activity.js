63013e88ec4020c6014bc33f08ae29c843381bd7
Blue Jessen <canealisblue@gmail.com>
2022-06-02 16:39:20 -0700

    Add average minutes active in a week

--- src/Activity.js
+++ src/Activity.js
@@ -1,8 +1,40 @@
 class Activity {
-  constructor() {
+  constructor(userInfo) {
+    this.activityRepo = userInfo;
+    this.mile = 5280; //1 mile in feet
+  }
 
+  findUser(userID) {
+    return this.activityRepo.find(user => user.userID === userID);
   };
-}
+
+  findAllUserData(userID) {
+    return this.activityRepo.filter(user => user.userID === userID);
+  };
+
+  milesWalked(userID, date, userRepo) {
+  let strideLength = userRepo.findUser(userID).strideLength;
+  let dataSet = this.findAllUserData(userID);
+  let steps = dataSet.find(day => day.date === date).numSteps;
+  return parseFloat(((steps*strideLength)/this.mile).toFixed(1));
+  }
+
+  minutesActive(userID, date) {
+      let dataSet = this.findAllUserData(userID);
+      return dataSet.find(day => day.date === date).minutesActive;
+  }
+
+  weeklyActivityAverage(userID, date) {
+      const dataSet = this.findAllUserData(userID);
+      const dayIndex = dataSet.findIndex(data => date === data.date);
+      const toDate = dataSet.slice(0 , (dayIndex+1));
+      const result = toDate.slice(-7).reduce((sum, day) => {
+        sum += day.minutesActive;
+        return sum;
+      }, 0);
+      return parseInt(result/7); 
+  }
+ }
 
 
   export default Activity;
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -19,12 +19,12 @@ describe('Activity', () => {
   });
 
   it('should be an instance of Activity', function () {
-    expect(hydrationRepo).to.be.an.instanceof(Activity);
+    expect(activityRepo).to.be.an.instanceof(Activity);
   });
 
   it('should be able to find the miles walked on a date', function () {
-    expect(activityRepo.milesWalked(7, "2020/01/22")).to.equal(1.8);
-    expect(activityRepo.milesWalked(7, "2020/01/21")).to.equal(1.3);
+    expect(activityRepo.milesWalked(7, "2020/01/22", userRepo)).to.equal(1.8);
+    expect(activityRepo.milesWalked(7, "2020/01/21", userRepo)).to.equal(1.3);
   });
 
   it('should be able to find the minutes active on a date', function () {
@@ -37,30 +37,30 @@ describe('Activity', () => {
     expect(activityRepo.weeklyActivityAverage(7, "2020/01/21")).to.equal(289);
   });
 
-  it('should be able to find if a user reached their step goal on a date', function () {
+  it.skip('should be able to find if a user reached their step goal on a date', function () {
     expect(activityRepo.stepGoalReached(7, "2020/01/22")).to.equal(false);
     expect(activityRepo.stepGoalReached(7, "2020/01/18")).to.equal(true);
   });
 
-  it('should be able to find all days a user exceeded their step goal', function () {
+  it.skip('should be able to find all days a user exceeded their step goal', function () {
     expect(activityRepo.daysStepGoalReached(7)).to.equal(["2020/01/14", "2020/01/18"]);
     expect(activityRepo.daysStepGoalReached(90)).to.equal(["2020/01/13"]);
   });
 
-  it('should be able to find the most stairs a user climbed in a day', function () {
+  it.skip('should be able to find the most stairs a user climbed in a day', function () {
     expect(activityRepo.maxStairs(7)).to.equal(105);
     expect(activityRepo.maxStairs(90)).to.equal(4);
   });
 
-  it('should be able to find the average stairs climbed on a day', function () {
+  it.skip('should be able to find the average stairs climbed on a day', function () {
     expect(activityRepo.averageStairs("2020/01/13")).to.equal(22);
   });
 
-  it('should be able to find the average steps taken on a day', function () {
+  it.skip('should be able to find the average steps taken on a day', function () {
     expect(activityRepo.averageSteps("2020/01/13")).to.equal(3267);
   });
 
-  it('should be able to find the average minutes active on a day', function () {
+  it.skip('should be able to find the average minutes active on a day', function () {
     expect(activityRepo.averageActivity("2020/01/13")).to.equal(452);
   });
 
